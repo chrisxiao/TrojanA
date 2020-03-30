1d382a39a085677b4c2442fb6cea4358bdbe6243
Chris Xiao <xiyouxiaochun@163.com>
2020-03-30 11:18:00 +0800

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = ["./pac", "./trojan", "./httpserver.py"]
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleShortVersionString': '0.1.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps'],
+}
+
+setup(
+    app=APP,
+    name='TrojanA',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['rumps']
+)
