02f402d873b7a2700c11d4f6f1bd35ce2636c10b
Chris Xiao <c.cx@foxmail.com>
2020-05-29 15:19:58 +0800

    update version number to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ OPTIONS = {
     'argv_emulation': True,
     'iconfile': 'icon.icns',
     'plist': {
-        'CFBundleShortVersionString': '0.1.0',
+        'CFBundleShortVersionString': '0.2.0',
         'LSUIElement': True,
     },
     'packages': ['rumps'],
